e79a46022aa0170556d6b19defc0c543cb4f1a4b
Julien Chaumond <chaumond@gmail.com>
2015-06-05 15:29:06 +0200

    Initial import

---
+++ cli.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var Mp4Convert = require('./index');
+
+var argv = process.argv.slice(2);
+
+var input  = __dirname+'/'+argv[0];
+var output = __dirname+'/'+argv[1];
+
+if (!fs.existsSync(input)) {
+	return console.log('Input file not found');
+}
+
+var convert = new Mp4Convert(input, output);
+convert.on('ffprobeCommand', function(cmd) {
+	console.log('Command', cmd);
+});
+convert.on('ffprobeOutput', function(json) {
+	console.log('ffprobe output');
+});
+convert.on('progress', function(p) {
+	console.log('Progress', p);
+});
+convert.on('done', function() {
+	console.log('Done');
+});
+convert.start();
+
+
+
+
